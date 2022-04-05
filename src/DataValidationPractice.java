4004d44a66b6fa72d33b9695443b35a17b7a882a
Grant Ales <grant.ales@yahoo.com>
2022-04-05 15:44:18 -0600

    finished cisID function

--- src/DataValidationPractice.java
+++ src/DataValidationPractice.java
@@ -1,20 +1,24 @@
-import java.io.FileWriter;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class DataValidationPractice {
 
-    public void isCorrectCisIDFormat(String cisID) {
-
+    public void isCorrectCisIDFormat(String cisID, String re, Pattern pt, Matcher mt) {
+        if (!mt.matches()) {
+            System.out.println("Write to file");
+        }
     }
 
-    public void isCorrectPIDFormat(String PID) {
+    public void isCorrectPIDFormat(String PID, String re, Pattern pt, Matcher mt) {
 
     }
 
     public void isCorrectDateFormat(String date) {
+    // Use this link to check date format -> https://stackoverflow.com/questions/20231539/java-check-the-date-format-of-current-string-is-according-to-required-format-or#:~:text=is%20not%20useful-,Show%20activity%20on%20this%20post.,in%20different%20format%20or%20invalid.%20%7D
 
     }
 
-    public void isAhnentafelValid(String ahnentafel, FileWriter fw) {
+    public void isAhnentafelValid(String ahnentafel) {
 
         try {
             int ahnentafelInt = Integer.parseInt(ahnentafel);
@@ -38,11 +42,19 @@ public class DataValidationPractice {
     public static void main(String[] args) {
         DataValidationPractice demo = new DataValidationPractice();
 
-        // Test if ahnentafel is working
-        demo.isAhnentafelValid("-10"); // Manually prints out message
-        demo.isAhnentafelValid("-10T"); // Error Message
-        demo.isAhnentafelValid("0"); // Error
-        demo.isAhnentafelValid("1"); // 1
-        demo.isAhnentafelValid("100"); // 100
+//        String re = "\\w\\w\\w\\w-\\w\\w\\w\\w";
+//        String text = "MMMDXLCHP";
+//        Pattern pt = Pattern.compile(re);
+//        Matcher mt = pt.matcher(text);
+//
+//        demo.isCorrectCisIDFormat(text, re, pt, mt);
+
+
+//        // Test if ahnentafel is working
+//        demo.isAhnentafelValid("-10"); // Manually prints out message
+//        demo.isAhnentafelValid("-10T"); // Error Message
+//        demo.isAhnentafelValid("0"); // Error
+//        demo.isAhnentafelValid("1"); // 1
+//        demo.isAhnentafelValid("100"); // 100
     }
 }
